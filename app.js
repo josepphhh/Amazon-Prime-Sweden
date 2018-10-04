ac1f8de52c839af7a30c19ce6b2968a0824bb297
YOusef Hamza <Joseph94h@hotmail.com>
2018-10-04 10:24:50 +0200

    Amazon-Prime-Sweden

---
+++ app.js
@@ -0,0 +1,7 @@
+var items = ["produkt"];
+document.getElementById("buybtn").innerHTML = items;
+
+function myFunction() {
+    items.push("produkt");
+    document.getElementById("buybtn").innerHTML = fruits;
+}
